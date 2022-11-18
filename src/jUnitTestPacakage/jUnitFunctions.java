5b250ffc22b0956a71f1606958b0ecc668af01ef
Kabir <Kabir@10.12.51.71>
2022-11-18 21:47:49 +0530

    jiuh

---
+++ src/jUnitTestPacakage/jUnitFunctions.java
@@ -0,0 +1,15 @@
+package jUnitTestPacakage;
+
+import java.util.Scanner;
+
+public class jUnitFunctions {
+	public static void main(String args[]) {
+		Scanner s =new Scanner(System.in);
+				int a=s.nextInt();
+		       int b=s.nextInt();
+	int sum=a+b;
+	System.out.println(sum);
+	}
+	
+
+}
